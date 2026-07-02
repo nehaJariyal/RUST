267849c7e58ebdeefa820a7c47660181e8e5a36e
Neha-ATR1305 <neha.kumari@antiersolutions.com>
2026-07-02 18:11:28 +0530

    print hello world

---
+++ 01_basics/hello_world.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, Rust!");
+
+
+     let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+}
+// Regular Comments
+
+// Line comments: Start with // and continue to the end of the line
+// Block comments: Enclosed in /* ... */ and can span multiple lines
